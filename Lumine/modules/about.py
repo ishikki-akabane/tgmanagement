bd60d68c71896450bb4c788b14f2bbe9b5a6363e
Ishikki Akabane <106568950+Ishikki-akabane-Dev@users.noreply.github.com>
2022-06-08 22:35:23 +0530

    Create about.py

---
+++ Lumine/modules/about.py
@@ -0,0 +1,11 @@
+__help__ = """
+** ✨ Ruka Sarashina - A Powerful Advance Telegram Group Manager Bot ✨ **
+Powerful Abilities
+•  ❤️
+• File To Link and URL Upload 📂
+• Youtube Downloader 🎵
+• 
+Developer [Ishikki Akabane](https://t.me/ishikki_akabane)
+Support Group [Support](https://t.me/suppportXD)
+"""
+__mod_name__ = "About"
